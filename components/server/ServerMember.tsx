7d894b7a353b7e1a56961107a22f14a3483c8da2
fardeen awais <fardeenjutt99@gmail.com>
2023-10-28 23:16:12 +0500

    Bug fixed: Params possibly null

--- components/server/ServerMember.tsx
+++ components/server/ServerMember.tsx
@@ -26,7 +26,7 @@ const ServerMember = ({ member, server}:ServerMemberProps) => {
   return (
     <button onClick={onClick} className={cn(`group p-2 rounded-md flex items-center gap-x-2 w-full hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50 transition mb-1`,params?.memberId===member.id &&"bg-zinc-700/20 dark:bg-zinc-700")}><UserAvatar className="h-3 w-3" src={member.profile.imageUrl}/>
     
-    <p className={cn(`font-semibold text-sm text-zinc-500 group-hover:text-zinc-600 transition`,params.memberId===member.id&&"text-primary dark:text-zinc-200 dark:group-hover:text-white")}>{member.profile.name}</p>{icon}</button>
+    <p className={cn(`font-semibold text-sm text-zinc-500 group-hover:text-zinc-600 transition`,params?.memberId===member.id&&"text-primary dark:text-zinc-200 dark:group-hover:text-white")}>{member.profile.name}</p>{icon}</button>
   )
 }
 
