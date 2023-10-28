42837cd8b39c1358748edafb5f6f1aa4c110882d
fardeen awais <fardeenjutt99@gmail.com>
2023-10-28 23:21:22 +0500

    Bug fix 02: params possibly null

--- components/server/server-channel.tsx
+++ components/server/server-channel.tsx
@@ -37,7 +37,7 @@ const ServerChannel = ({ channel, server, role }: ServerChannelProps) => {
     return (
         <button onClick={onClick} className={cn("group px-2 py-2 rounded-md flex items-center gap-x-2 w-full hover:bg-zinc-700 dark:hover:bg-zinc-700/30 transition-all mb-1", params?.channelId === channel.id && "bg-zinc-700/20 dark:bg-zinc-700")}>
             <Icon className="flex-shrink-0 w-4 h-4 text-zinc-500 hover:text-zinc-400" />
-            <p className={cn(`line-clamp-1 font-semibold text-sm text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 group-hover:text-zinc-300 transition-all`, params.channelId === channel.id && "text-primary dark:text-zinc-200 group-hover:text-white")}>
+            <p className={cn(`line-clamp-1 font-semibold text-sm text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 group-hover:text-zinc-300 transition-all`, params?.channelId === channel.id && "text-primary dark:text-zinc-200 group-hover:text-white")}>
                 {channel.name}
             </p>
             {channel.name !== "general" && role != MemberRole.GUEST && (
